0344bf98f9024cc6e9de418059bd132f1e1be0c0
Mahalakshmi2308 <66907647+Mahalakshmi2308@users.noreply.github.com>
2021-02-01 21:26:35 +0530

    Add files via upload

---
+++ swippingFile.py
@@ -0,0 +1,16 @@
+def countWordFromFile():
+  fileName = input("enter the file name : ")
+  
+  numberOfWords = 0
+
+  file = open(fileName, 'r')
+  for line in file:
+    words = line.split()
+
+    numberOfWords = numberOfWords+len(words)
+
+  print("number of words : ")
+  print(numberOfWords)
+
+  
+countWordFromFile()
